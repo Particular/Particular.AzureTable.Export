ecc4730d0c1c60081575b7813ab3c706cd90e682
Dan Kent <83468000+kentdr@users.noreply.github.com>
2021-08-02 16:59:35 -0400

    Copied the code over from CosmosDB persistence release 1.0.

---
+++ src/AzureStorageSagaExporter/ApplicationOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace NServiceBus.Persistence.CosmosDB.AzureStorageSagaExporter
+{
+    class ApplicationOptions
+    {
+        public const string SagaDataName = "sagadataname";
+        public const string ConnectionString = "connectionstring";
+    }
+}

0545f79ecd3f4467425c190b3cc452fd4069ac2d
Dan Kent <83468000+kentdr@users.noreply.github.com>
2021-08-12 16:07:30 -0400

    Adding AzureTable4 tests.

---
+++ src/Tests.AzureTable4/BaseEndpoint.cs
@@ -0,0 +1,27 @@
+﻿namespace Tests.AzureTable4
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.AcceptanceTesting.Support;
+
+    public class BaseEndpoint : IEndpointSetupTemplate
+    {
+        public virtual Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomizationConfiguration, Action<EndpointConfiguration> configurationBuilderCustomization)
+        {
+            var endpointConfiguration = new EndpointConfiguration(endpointCustomizationConfiguration.EndpointName);
+
+            endpointConfiguration.Recoverability()
+                .Delayed(delayed => delayed.NumberOfRetries(0))
+                .Immediate(immediate => immediate.NumberOfRetries(0));
+
+            endpointConfiguration.UseTransport(new AcceptanceTestingTransport());
+
+            endpointConfiguration.RegisterComponentsAndInheritanceHierarchy(runDescriptor);
+
+            configurationBuilderCustomization(endpointConfiguration);
+
+            return Task.FromResult(endpointConfiguration);
+        }
+    }
+}
